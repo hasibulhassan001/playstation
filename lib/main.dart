d52b0c956f337669325b160b9419bab8162d9b24
hasibulhassan001 <hasibulhassanhotstar001@gmail.com>
2023-12-02 19:12:09 +0800

    Read from JSON
    Get data from JSON and create gamelist to show in the group list view

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,67 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:group_list_view/group_list_view.dart';
+import 'dart:developer';
 
-void main() => runApp(const MyApp());
+
+void main() => runApp(MyApp());
+
+List<String> platforms = [];
+
+Map<String, List> games = {};
 
 Map<String, List> _elements = {
   'Team A': ['Klay Lewis', 'Ehsan Woodard', 'River Bains'],
   'Team B': ['Toyah Downs', 'Tyla Kane'],
   'Team C': ['Marcus Romero', 'Farrah Parkes', 'Fay Lawson', 'Asif Mckay'],
-  'Team D': [
-    'Casey Zuniga',
-    'Ayisha Burn',
-    'Josie Hayden',
-    'Kenan Walls',
-    'Mario Powers'
-  ],
+  'Team D': ['Casey Zuniga', 'Ayisha Burn', 'Josie Hayden', 'Kenan Walls', 'Mario Powers'],
   'Team Q': ['Toyah Downs', 'Tyla Kane', 'Toyah Downs'],
+  'Team X': ['Toyah Downs', 'Kenan Walls', 'Mario Powers'],
+  'Team Z': ['Toyah Downs', 'Tyla Kane', 'Kenan Walls', 'Mario Powers'],
 };
 
+void _makeGameList() {
+  List name = ['Klay Lewis', 'Ehsan Woodard', 'River Bains'];
+
+  games = {for (var item in platforms) '$item' : name};
+
+  log("GameList = ${games.toString()}");
+}
+
+// ignore: must_be_immutable
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  List _items = [];
+  String _name = "";
+
+  // Fetch content from the json file
+  Future<void> readJson() async {
+  final String response = await rootBundle.loadString('assets/getgamelist.json');
+  final data = await json.decode(response);
+   _items = data["results"];
+
+   _name = data["count"].toString();
+
+   _gameList(_items);
+   _logUser("Name = ${_name.toString()}");
+  }
 
   @override
   Widget build(BuildContext context) {
+    
+    _logUser("Main App Called");
+    readJson();
+
     return MaterialApp(
-      title: 'Group List View Demo',
+      title: 'PlayStation Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Group List View Demo'),
+          title: const Text('PlayStation Demo'),
         ),
         body: GroupListView(
           sectionsCount: _elements.keys.toList().length,
@@ -73,6 +105,9 @@ class MyApp extends StatelessWidget {
             style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
           ),
           trailing: const Icon(Icons.arrow_forward_ios),
+          onTap: () {
+            _makeGameList();
+          },
         ),
       ),
     );
@@ -86,5 +121,25 @@ class MyApp extends StatelessWidget {
     }
 
     return buffer.toString().substring(0, split.length);
+  }
+
+  void _gameList(List game) {
+    var i = 0, j = 0;
+
+    log("GameList Size = ${game.length}");
+    log("platform = ${game[0]["platforms"][0]["platform"]["name"].toString()}");
+
+    for (i = 0;  i < game.length; i++) {
+      for (j = 0; j < game[i]["platforms"].length; j++) {
+        platforms.add(game[i]["platforms"][j]["platform"]["name"]);
+      }
+      j = 0;
+    }
+    platforms = platforms.toSet().toList();
+    log(platforms.toString());
+  }
+
+  void _logUser(String user) {
+    log(user);
   }
 }
